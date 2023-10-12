8291f7af7e1b73e0ef1a40896783fd5b2dc79f4f
Leo <deutschleonardo@gmail.com>
2023-10-12 13:55:22 -0500

    setting up the canvas index file and its additional components

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,3 +1,10 @@
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+
 
 const Canvas = () => {
   return (
