afcf0636e958f7aff005fc0b69f2713be679a355
Leo <deutschleonardo@gmail.com>
2023-10-12 17:43:08 -0500

    implemented several functions to handle decals and tabs

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -13,7 +13,7 @@ import { AIPicker, ColorPicker, FilePicker, CustomButton, Tab } from "../compone
 const Customizer = () => {
   const snap = useSnapshot(state);
   const [file, setFile] = useState('');
-  const [promt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState('');
   const [generatingImg, setGeneratingImg] = useState(false);
   const [activeEditorTab, setActiveEditorTab] = useState('');
   const [activeFilterTab, setActiveFilterTab] = useState({ logoShirt: true, stylishShirt: false});
@@ -24,13 +24,46 @@ const Customizer = () => {
       case 'colorpicker':
         return <ColorPicker />
       case 'filepicker':
-        return <FilePicker />
+        return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
+        />
       case 'aipicker':
         return <AIPicker />
       
       default:
         return null;
     }
+  }
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+    state[decalType.stateProperty] = result;
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case 'logoShirt':
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case 'stylishShirt':
+        state.isFullTexture = !activeFilterTab[tabName];
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab('');
+      })
   }
 
   return (
