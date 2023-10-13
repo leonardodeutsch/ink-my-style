3098029c896904705a946a75d61896c80bad7355
Leo <deutschleonardo@gmail.com>
2023-10-13 14:29:07 -0500

    implemented AI picker by adding post request to Open AI

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,10 +1,12 @@
-import express, { Router, application } from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 import OpenAI from 'openai';
 
 dotenv.config();
 const router = express.Router();
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES"})
@@ -13,13 +15,13 @@ router.route('/').get((req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json'
     })
-    const image = response.data.data[0].b64_json;
+    const image = response.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
